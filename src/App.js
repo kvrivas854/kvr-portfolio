e07ee5983bff474a3ce9143ed766a62e53422e0f
Kelly Rivas <kvrivas854@mail.cfcc.com>
2020-10-04 13:08:25 -0400

    portfolio-project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/index";
+import Home from "./components/Home/index";
+import About from "./components/pages/About";
+import Project from "./components/Project/index";
+import Contact from "./components/Contact/index";
+import Experience from "./components/Experience/index";
+
+
+
+class App extends Component {
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Project} />
+        <Route exact path="/contact" component={Contact} />
+        <Route path="/experience" component={Experience} />
+      </div>
+    </Router>
     </div>
+    
   );
 }
-
+    }
 export default App;
